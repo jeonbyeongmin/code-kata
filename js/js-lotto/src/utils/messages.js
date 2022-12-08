ba6cd25c7c03f6b896033a4efb7a565818df4850
jeonbyeongmin <qudals7613@gmail.com>
2022-12-08 23:37:55 +0900

    feat: view, 상수 값 정리

---
+++ js/js-lotto/src/utils/messages.js
@@ -0,0 +1,30 @@
+const NEW_LINE = '\n';
+
+const INPUT_MESSAGE = {
+  PRICE: '구매금액을 입력해 주세요.',
+  NUMBERS: '당첨 번호를 입력해 주세요.',
+  BONUS: '보너스 번호를 입력해 주세요.',
+};
+
+const OUTPUT_MESSAGE = {
+  PURCHASE_COUNT: (count) => `${count}개를 구매했습니다.`,
+  STATS: '당첨 통계',
+  DIVIDER: '---',
+  FIFTH: (count) => `3개 일치 (5,000원) - ${count}개`,
+  FOURTH: (count) => `4개 일치 (50,000원) - ${count}개`,
+  THIRD: (count) => `5개 일치 (1,500,000원) - ${count}개`,
+  SECOND: (count) => `5개 일치, 보너스 볼 일치 (30,000,000원) - ${count}개`,
+  FIRST: (count) => `6개 일치 (2,000,000,000원) - ${count}개`,
+  RATE: (rate) => `총 수익률은 ${rate}%입니다.`,
+};
+
+const ERROR_MESSAGE = {
+  BOUND: '[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.',
+};
+
+module.exports = {
+  NEW_LINE,
+  INPUT_MESSAGE,
+  OUTPUT_MESSAGE,
+  ERROR_MESSAGE,
+};
---
+++ js/js-lotto/src/view/OutputView.js
@@ -0,0 +1,45 @@
+const { Console } = require('@woowacourse/mission-utils');
+const { OUTPUT_MESSAGE } = require('../utils/messages');
+
+const OutputView = {
+  /**
+   * 구입금액에 따른 로또 구매 개수를 출력한다
+   * @param {number} count
+   */
+  printPurchaseCount(count) {
+    Console.print(OUTPUT_MESSAGE.PURCHASE_COUNT(count));
+  },
+
+  /**
+   * 로또를 출력한다
+   * @param {number[]} numbers
+   */
+  printNumbers(numbers) {
+    Console.print(`[${numbers.join(', ')}]`);
+  },
+
+  /**
+   * 당첨 통계를 출력한다
+   * @param {number[]} stats
+   */
+  printStats(stats) {
+    const [fifth, fourth, third, second, first] = stats;
+    Console.print(OUTPUT_MESSAGE.STATS);
+    Console.print(OUTPUT_MESSAGE.DIVIDER);
+    Console.print(OUTPUT_MESSAGE.FIFTH(fifth));
+    Console.print(OUTPUT_MESSAGE.FOURTH(fourth));
+    Console.print(OUTPUT_MESSAGE.THIRD(third));
+    Console.print(OUTPUT_MESSAGE.SECOND(second));
+    Console.print(OUTPUT_MESSAGE.FIRST(first));
+  },
+
+  /**
+   * 당첨 통계를 출력한다
+   * @param {number} rate
+   */
+  printRate(rate) {
+    Console.print(OUTPUT_MESSAGE.RATE(rate));
+  },
+};
+
+module.exports = OutputView;
