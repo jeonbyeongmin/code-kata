f71558643f170d8fe5bb57e0de4df8d387af8185
jeonbyeongmin <qudals7613@gmail.com>
2022-12-10 19:09:22 +0900

    fix: InputView 개행 추가

--- js/js-lotto/src/view/InputView.js
+++ js/js-lotto/src/view/InputView.js
@@ -3,15 +3,15 @@ const { INPUT_MESSAGE, NEW_LINE } = require('../utils/messages');
 
 const InputView = {
   readPrice(callback) {
-    Console.readLine(INPUT_MESSAGE.PRICE, callback);
+    Console.readLine(INPUT_MESSAGE.PRICE + NEW_LINE, callback);
   },
 
   readNumbers(callback) {
-    Console.readLine(INPUT_MESSAGE.NUMBERS + NEW_LINE, callback);
+    Console.readLine(NEW_LINE + INPUT_MESSAGE.NUMBERS + NEW_LINE, callback);
   },
 
   readBonus(callback) {
-    Console.readLine(INPUT_MESSAGE.BONUS + NEW_LINE, callback);
+    Console.readLine(NEW_LINE + INPUT_MESSAGE.BONUS + NEW_LINE, callback);
   },
 };
 
